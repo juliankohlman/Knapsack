84a1cb025e18acaa016ab120a1a61c758f53a65e
Julian Kohlman <juliankohlman@gmail.com>
2018-04-18 18:49:04 -0400

    Kind of parsing the string into row-cost-val

--- src/knapsack.c
+++ src/knapsack.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 
 int main () 
 {
   char *debugFile = "../data/small1.txt";
   printf("file is %s\n", debugFile);
 
-  char buff[255];
+  char buff[256];
   FILE *fp;
   fp = fopen(debugFile, "r");
 
-  while (fgets(buff, 255, (FILE *)fp) != NULL) {
-    printf("%s\n", buff);
+  char * row;
+  char * cost;
+  char * val;
+
+  while (fgets(buff, 256, (FILE *)fp) != NULL) {
+    row = strtok(&buff[0], "");
+    cost = strtok(&buff[2], "");
+    val = strtok(&buff[5], "");
+    printf("Row: %s Cost: %s Value: %s", row, cost, val);
+
+    // sscanf(buff, "%255s", row);
   }
 
   fclose(fp);
-
   int debugSize = 100;
 }
